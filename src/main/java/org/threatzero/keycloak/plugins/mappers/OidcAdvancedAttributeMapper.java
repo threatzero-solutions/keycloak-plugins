afc193974e7985ceb1f7ddfff8bb7e84013e2673
Jared Gibson <jared.gibson@live.com>
2024-11-20 14:51:24 -0700

    Refactor config properties and use "exact" instead of "plain"

--- src/main/java/org/threatzero/keycloak/plugins/mappers/OidcAdvancedAttributeMapper.java
+++ src/main/java/org/threatzero/keycloak/plugins/mappers/OidcAdvancedAttributeMapper.java
@@ -1,5 +1,6 @@
 package org.threatzero.keycloak.plugins.mappers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -28,11 +29,63 @@ public class OidcAdvancedAttributeMapper extends AbstractClaimMapper {
   private static final Set<IdentityProviderSyncMode> IDENTITY_PROVIDER_SYNC_MODES =
       new HashSet<>(Arrays.asList(IdentityProviderSyncMode.values()));
 
+  private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
   private static final String CLAIM_NAME = "attribute.name";
   private static final String MATCH_PATTERNS = "patterns";
   private static final String DEFAULT_VALUE = "default.value";
   private static final String PATTERN_TYPE = "pattern.type";
   private static final String ATTRIBUTE_NAME = "user.attribute";
+
+  static {
+    // CLAIM property
+    ProviderConfigProperty claimProperty = new ProviderConfigProperty();
+    claimProperty.setName(CLAIM_NAME);
+    claimProperty.setLabel("Claim");
+    claimProperty.setType(ProviderConfigProperty.STRING_TYPE);
+    claimProperty.setHelpText(
+        "The name of the claim to match against. You can reference nested claims using a '.',"
+            + " i.e. 'address.locality'. To use dot (.) literally, escape it with backslash."
+            + " (\\.)");
+    configProperties.add(claimProperty);
+
+    // MATCH_PATTERNS property
+    ProviderConfigProperty matchPatternsProperty = new ProviderConfigProperty();
+    matchPatternsProperty.setName(MATCH_PATTERNS);
+    matchPatternsProperty.setLabel("Match Patterns");
+    matchPatternsProperty.setType(ProviderConfigProperty.MAP_TYPE);
+    matchPatternsProperty.setHelpText(
+        "The patterns and the corresponding values to assign to the attribute upon match. The"
+            + " key should be a pattern of one of the types below and the value should be the"
+            + " value that is assigned to the user attribute if the pattern matches.");
+    configProperties.add(matchPatternsProperty);
+
+    // DEFAULT_VALUE property
+    ProviderConfigProperty defaultValueProperty = new ProviderConfigProperty();
+    defaultValueProperty.setName(DEFAULT_VALUE);
+    defaultValueProperty.setLabel("Default Value");
+    defaultValueProperty.setType(ProviderConfigProperty.STRING_TYPE);
+    defaultValueProperty.setHelpText("The value to use if no claims match.");
+    configProperties.add(defaultValueProperty);
+
+    // PATTERN_TYPE property
+    ProviderConfigProperty patternTypeProperty = new ProviderConfigProperty();
+    patternTypeProperty.setName(PATTERN_TYPE);
+    patternTypeProperty.setLabel("Pattern Type");
+    patternTypeProperty.setType(ProviderConfigProperty.LIST_TYPE);
+    patternTypeProperty.setHelpText(
+        "Algorithm used to match claims. Supported types: exact, regex, glob.");
+    patternTypeProperty.setOptions(List.of("exact", "regex", "glob"));
+    patternTypeProperty.setDefaultValue("exact");
+    configProperties.add(patternTypeProperty);
+
+    // ATTRIBUTE_NAME property
+    ProviderConfigProperty attributeNameProperty = new ProviderConfigProperty();
+    attributeNameProperty.setName(ATTRIBUTE_NAME);
+    attributeNameProperty.setLabel("User Attribute Name");
+    attributeNameProperty.setType(ProviderConfigProperty.USER_PROFILE_ATTRIBUTE_LIST_TYPE);
+    attributeNameProperty.setHelpText("The name of the user attribute to assign the value to.");
+  }
 
   @Override
   public void close() {}
@@ -60,44 +113,7 @@ public class OidcAdvancedAttributeMapper extends AbstractClaimMapper {
 
   @Override
   public List<ProviderConfigProperty> getConfigProperties() {
-    return List.of(
-        new ProviderConfigProperty(
-            CLAIM,
-            "Claim",
-            "The name of the claim to match against. You can reference nested claims using a '.',"
-                + " i.e. 'address.locality'. To use dot (.) literally, escape it with backslash."
-                + " (\\.)",
-            ProviderConfigProperty.STRING_TYPE,
-            ""),
-        new ProviderConfigProperty(
-            MATCH_PATTERNS,
-            "Match Patterns",
-            "The patterns and the corresponding values to assign to the attribute upon match. The"
-                + " key should be a pattern of one of the types below and the value should be the"
-                + " value that is assigned to the user attribute if the pattern matches.",
-            ProviderConfigProperty.MAP_TYPE,
-            Map.of()),
-        new ProviderConfigProperty(
-            DEFAULT_VALUE,
-            "Default Value",
-            "The value to use if no claims match.",
-            ProviderConfigProperty.STRING_TYPE,
-            ""),
-        new ProviderConfigProperty(
-            PATTERN_TYPE,
-            "Pattern Type",
-            "Algorithm used to match claims. Supported types: plain, regex, glob.",
-            ProviderConfigProperty.LIST_TYPE,
-            "plain",
-            "plain",
-            "regex",
-            "glob"),
-        new ProviderConfigProperty(
-            ATTRIBUTE_NAME,
-            "User Attribute Name",
-            "User attribute to assign claim value to.",
-            ProviderConfigProperty.USER_PROFILE_ATTRIBUTE_LIST_TYPE,
-            ""));
+    return configProperties;
   }
 
   @Override
@@ -178,7 +194,7 @@ public class OidcAdvancedAttributeMapper extends AbstractClaimMapper {
     }
 
     String attributeName = mapperModel.getConfig().get(ATTRIBUTE_NAME);
-    if (attributeName != null) {
+    if (attributeName != null && attributeValue != null && !attributeValue.isBlank()) {
       user.setSingleAttribute(attributeName, attributeValue);
     }
   }
--- src/main/java/org/threatzero/keycloak/plugins/mappers/SamlAdvancedAttributeMapper.java
+++ src/main/java/org/threatzero/keycloak/plugins/mappers/SamlAdvancedAttributeMapper.java
@@ -1,5 +1,6 @@
 package org.threatzero.keycloak.plugins.mappers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -29,11 +30,62 @@ public class SamlAdvancedAttributeMapper extends AbstractIdentityProviderMapper 
   private static final Set<IdentityProviderSyncMode> IDENTITY_PROVIDER_SYNC_MODES =
       new HashSet<>(Arrays.asList(IdentityProviderSyncMode.values()));
 
+  private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
   private static final String CLAIM_NAME = "attribute.name";
   private static final String MATCH_PATTERNS = "patterns";
   private static final String DEFAULT_VALUE = "default.value";
   private static final String PATTERN_TYPE = "pattern.type";
   private static final String ATTRIBUTE_NAME = "user.attribute";
+
+  static {
+    // CLAIM property
+    ProviderConfigProperty claimProperty = new ProviderConfigProperty();
+    claimProperty.setName(CLAIM_NAME);
+    claimProperty.setLabel("Attribute");
+    claimProperty.setType(ProviderConfigProperty.STRING_TYPE);
+    claimProperty.setHelpText(
+        "The name of the attribute to match against. This can either be the name or the"
+            + " friendly name.");
+    configProperties.add(claimProperty);
+
+    // MATCH_PATTERNS property
+    ProviderConfigProperty matchPatternsProperty = new ProviderConfigProperty();
+    matchPatternsProperty.setName(MATCH_PATTERNS);
+    matchPatternsProperty.setLabel("Match Patterns");
+    matchPatternsProperty.setType(ProviderConfigProperty.MAP_TYPE);
+    matchPatternsProperty.setHelpText(
+        "The patterns and the corresponding values to assign to the attribute upon match. The"
+            + " key should be a pattern of one of the types below and the value should be the"
+            + " value that is assigned to the user attribute if the pattern matches.");
+    configProperties.add(matchPatternsProperty);
+
+    // DEFAULT_VALUE property
+    ProviderConfigProperty defaultValueProperty = new ProviderConfigProperty();
+    defaultValueProperty.setName(DEFAULT_VALUE);
+    defaultValueProperty.setLabel("Default Value");
+    defaultValueProperty.setType(ProviderConfigProperty.STRING_TYPE);
+    defaultValueProperty.setHelpText("The value to use if no claims match.");
+    configProperties.add(defaultValueProperty);
+
+    // PATTERN_TYPE property
+    ProviderConfigProperty patternTypeProperty = new ProviderConfigProperty();
+    patternTypeProperty.setName(PATTERN_TYPE);
+    patternTypeProperty.setLabel("Pattern Type");
+    patternTypeProperty.setType(ProviderConfigProperty.LIST_TYPE);
+    patternTypeProperty.setHelpText(
+        "Algorithm used to match claims. Supported types: exact, regex, glob.");
+    patternTypeProperty.setOptions(List.of("exact", "regex", "glob"));
+    patternTypeProperty.setDefaultValue("exact");
+    configProperties.add(patternTypeProperty);
+
+    // ATTRIBUTE_NAME property
+    ProviderConfigProperty attributeNameProperty = new ProviderConfigProperty();
+    attributeNameProperty.setName(ATTRIBUTE_NAME);
+    attributeNameProperty.setLabel("User Attribute Name");
+    attributeNameProperty.setType(ProviderConfigProperty.USER_PROFILE_ATTRIBUTE_LIST_TYPE);
+    attributeNameProperty.setHelpText("The name of the user attribute to assign the value to.");
+  }
 
   @Override
   public void close() {}
@@ -61,43 +113,7 @@ public class SamlAdvancedAttributeMapper extends AbstractIdentityProviderMapper 
 
   @Override
   public List<ProviderConfigProperty> getConfigProperties() {
-    return List.of(
-        new ProviderConfigProperty(
-            CLAIM_NAME,
-            "Attribute",
-            "The name of the attribute to match against. This can either be the name or the"
-                + " friendly name.",
-            ProviderConfigProperty.STRING_TYPE,
-            ""),
-        new ProviderConfigProperty(
-            MATCH_PATTERNS,
-            "Match Patterns",
-            "The patterns and the corresponding values to assign to the attribute upon match. The"
-                + " key should be a pattern of one of the types below and the value should be the"
-                + " value that is assigned to the user attribute if the pattern matches.",
-            ProviderConfigProperty.MAP_TYPE,
-            Map.of()),
-        new ProviderConfigProperty(
-            DEFAULT_VALUE,
-            "Default Value",
-            "The value to use if no claims match.",
-            ProviderConfigProperty.STRING_TYPE,
-            ""),
-        new ProviderConfigProperty(
-            PATTERN_TYPE,
-            "Pattern Type",
-            "Algorithm used to match claims. Supported types: plain, regex, glob.",
-            ProviderConfigProperty.LIST_TYPE,
-            "plain",
-            "plain",
-            "regex",
-            "glob"),
-        new ProviderConfigProperty(
-            ATTRIBUTE_NAME,
-            "User Attribute Name",
-            "User attribute to assign claim value to.",
-            ProviderConfigProperty.USER_PROFILE_ATTRIBUTE_LIST_TYPE,
-            ""));
+    return configProperties;
   }
 
   @Override
@@ -143,7 +159,7 @@ public class SamlAdvancedAttributeMapper extends AbstractIdentityProviderMapper 
   protected boolean valueMatches(String pattern, String value, String patternType) {
     if (value != null) {
       switch (patternType) {
-        case "plain":
+        case "exact":
           return pattern.equals(value);
         case "regex":
           return Pattern.compile(pattern).matcher((String) value).matches();
@@ -202,7 +218,7 @@ public class SamlAdvancedAttributeMapper extends AbstractIdentityProviderMapper 
     }
 
     String attributeName = mapperModel.getConfig().get(ATTRIBUTE_NAME);
-    if (attributeName != null) {
+    if (attributeName != null && attributeValue != null && !attributeValue.isBlank()) {
       user.setSingleAttribute(attributeName, attributeValue);
     }
   }
